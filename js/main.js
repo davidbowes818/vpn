d9ca8cbc8a0d3c36240c05b448c17989f8074554
iloveket <davidrb2017@outlook.com>
2019-10-21 17:06:10 +0100

    Added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // UI VARS
 
-const accordions = document.getElementsByClassName("accordion");
+const accordions = document.getElementsByClassName('accordion');
 
 // ACCORDION
 
 for (let i = 0; i < accordions.length; i++) {
   accordions[i].onclick = function() {
-    this.classList.toggle("accordion-active");
+    this.classList.toggle('accordion-active');
     const content = this.nextElementSibling;
     if (content.style.maxHeight) {
       // ACCORDION OPEN
@@ -18,4 +18,17 @@ for (let i = 0; i < accordions.length; i++) {
   };
 }
 
+$('.landing a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
 
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
